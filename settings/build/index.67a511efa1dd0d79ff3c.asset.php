282430edc05a39efd043cb33e174a6c69abec057
domenico.gangemi <domenico.gangemi@team.blue>
2025-07-29 17:09:47 +0200

    Remove deleted

--- settings/build/index.67a511efa1dd0d79ff3c.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '67a511efa1dd0d79ff3c');
